a9c32b9ec7b4bb820022e80e5c3c6a20d801300c
VaibhavMatere <vpm00718897@gmail.com>
2024-07-22 20:04:00 +0530

    feat : Dark mode added

---
+++ src/components/layout/toggle-theme.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from "next-themes";
+import { Button } from "../ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export const ToggleTheme = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <Button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      size="sm"
+      variant="ghost"
+      className="w-full justify-start"
+    >
+      <div className="flex gap-2 dark:hidden">
+        <Moon className="size-5" />
+        <span className="block lg:hidden"> Dark </span>
+      </div>
+
+      <div className="dark:flex gap-2 hidden">
+        <Sun className="size-5" />
+        <span className="block lg:hidden">Light</span>
+      </div>
+
+      <span className="sr-only">Set theme</span>
+    </Button>
+  );
+};
