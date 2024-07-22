50d26f5b0494d3f5e24da4ce7c012284b8414d4f
VaibhavMatere <vpm00718897@gmail.com>
2024-07-22 16:40:41 +0530

    navbar filename typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import { Inter } from "next/font/google";
 import { HeroSection } from "@/components/HeroSection";
 import { CompaniesList } from "@/components/CompaniesList";
