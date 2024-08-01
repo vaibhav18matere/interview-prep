bcd8d5a99f1b39e5622d6dac7a041ed3ebf762d1
VaibhavMatere <vpm00718897@gmail.com>
2024-08-01 11:42:00 +0530

    commented icon components

--- src/pages/theory/index.tsx
+++ src/pages/theory/index.tsx
@@ -3,12 +3,12 @@ import { Icon } from "@/components/ui/icon";
 import { topics } from "@/lib/data";
 import Link from "next/link";
 import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact } from "react-icons/fa";
-const iconMapping: { [key: string]: React.ComponentType } = {
-  html5: FaHtml5,
-  css3: FaCss3Alt,
-  javascript: FaJsSquare,
-  react: FaReact,
-};
+// const iconMapping: { [key: string]: React.ComponentType } = {
+//   html5: FaHtml5,
+//   css3: FaCss3Alt,
+//   javascript: FaJsSquare,
+//   react: FaReact,
+// };
 
 const theory = () => {
   return (
@@ -19,16 +19,16 @@ const theory = () => {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {topics.map(({ id, title, description, icon, href }, index) => {
-            const IconComponent = iconMapping[icon];
+            // const IconComponent = iconMapping[icon];
             return (
               <Link href={href} key={id}>
                 <Card className="h-full border border-gray-200 shadow-lg hover:shadow-xl transition-all duration-300 bg-muted/50 dark:bg-card transform hover:-translate-y-1 hover:scale-105">
                   <CardHeader className="flex flex-col justify-center items-center">
                     <div className="bg-primary/20 p-4 rounded-full ring-8 ring-primary/10 mb-4 transition-all duration-300 hover:ring-primary/20">
-                      <IconComponent
+                      {/* <IconComponent
                         size={40}
                         className="text-primary transition-all duration-300 hover:text-primary-dark"
-                      />
+                      /> */}
                     </div>
                     <CardTitle className="text-center text-xl font-bold text-gray-800 dark:text-white transition-all duration-300 hover:text-primary">
                       {title}
