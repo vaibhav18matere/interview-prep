04413525c822d5946c2e11f270fd1972a5a5e25c
prathambhalke <prathameshbhalke125@gmail.com>
2024-08-03 18:20:22 +0530

    -added the breadcrumbs for better accessibility -added the theory route path in navbar -created a custom hook for setting the document title as per page

--- src/pages/theory/css/index.tsx
+++ src/pages/theory/css/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AccordionDemo from "@/components/Accordion";
 import { CSSTheory } from "@/lib/TheoryQueData/cssTheoryData";
+import useDynamicTitle from "@/hooks/useDynamicTitle";
 
 const index = () => {
+  useDynamicTitle("Theory");
   return (
-    <section className="mx-auto py-15 md:py-18">
+    <section className="mx-auto py-15 md:py-16">
       <AccordionDemo data={CSSTheory}  heading={"CSS"}/>
     </section>
   );
